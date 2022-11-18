62b5dd727837852dbc55beb91882e3be076facad
Joan Renfroe <97361642+Jeal3R5@users.noreply.github.com>
2022-11-17 18:10:47 -0800

    improve CSS, make weight and temp random for both us and uk

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ***Silly Story Generator***
 
 This project was obtained from MDN as part of a lesson in the odinProject
+SPA that generates a short silly paragraph.
 
 Tech Used: 
 * HTML
@@ -10,5 +11,5 @@ Tech Used:
 * GitHub
 
 Future improvements: 
-* Add a variety of options in the arrays for an even more diverse story - perhaps use an API to make it fun
+* Perhaps find an API to use to randomly generate different portions of the story
 * Improve CSS further
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const customName = document.getElementById("customname");
 const randomize = document.querySelector(".randomize");
 const story = document.querySelector(".story");
 
+
 function randomValueFromArray(arr) {
   const random = Math.floor(Math.random() * arr.length);
   return arr[random];
@@ -10,13 +11,36 @@ function randomValueFromArray(arr) {
 
 //Contain strings inside variables
 const storyText =
-  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Time"];
-const insertY = ["the soup kitchen", "Disneyland", "Grandma's House"];
+  "It was 94 degrees fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised - :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = [
+  "Willy the Goblin",
+  "Big Daddy",
+  "Father Time",
+  "Pop Tart",
+  "Ruby the Stone Carver",
+  "Luna the Crooner",
+  "Avari the Artsy",
+  "Emily the 1st",
+];
+
+const insertY = [
+  "the soup kitchen",
+  "Disneyland",
+  "Grandma's House",
+  "the soccer field",
+  "dance class",
+  "the pool",
+];
+
 const insertZ = [
   "spontaneously combusted",
   "melted into a puddle on the sidewalk",
   "turned into a slug and crawled away",
+  "got toasted",
+  "joined a rock band",
+  "flew to the moon",
+  "an alien spaceship beamed them up Scotty",
 ];
 
 //Section 3 - Event Listener and Partial function definition
@@ -43,13 +67,31 @@ function result() {
     newStory = newStory.replaceAll("Bob", name);
   }
 
+  //Adding a text box for the temperature ... hmm....or show we make it random? Make it random generature temp and weight
+
   if (document.getElementById("uk").checked) {
-    const weight = `${Math.round(300 / 14)} stone`;
+    //    const weight = `${Math.round(300 / 14)} stone`;
+    const randomWeight = Math.floor(Math.random() * 100);
+    const weight = randomWeight / 14 + " stone";
 
-    const temperature = `${(Math.round(94 - 32) * 5) / 9} centigrade`;
+    //const temperature = `${(Math.round(94 - 32) * 5 / 9)} centigrade`;
+    const randomTemp = Math.floor(Math.random() * 100);
+    const temperature = ((randomTemp - 32) * 5) / 9 + " degrees celsius";
 
-    newStory = newStory.replaceAll("94 fahrenheit", temperature);
+    newStory = newStory.replaceAll("94 degrees fahrenheit", temperature);
     newStory = newStory.replaceAll("300 pounds", weight);
+  }
+
+  //adding logic to get the random temp and weight in (us) right now it only works in Uk
+  if (document.getElementById("us").checked) {
+    const randomWeight = Math.floor(Math.random() * 100);
+    const weight = `${randomWeight} pounds`;
+
+    const randomTemp = Math.floor(Math.random() * 100);
+    const temperature = `${randomTemp} fahrenheit`;
+
+    newStory = newStory.replaceAll("300 pounds", weight);
+    newStory = newStory.replaceAll("94 degrees fahrenehit", temperature);
   }
 
   story.textContent = newStory;
